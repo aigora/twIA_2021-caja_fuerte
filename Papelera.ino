3e6a82ea4e2c302a3f3125021cf5802e7f857bd6
raquelclementebastante <80747527+raquelclementebastante@users.noreply.github.com>
2021-05-09 19:58:32 +0200

    Update Papelera.ino

--- Papelera.ino
+++ Papelera.ino
@@ -1,16 +1,27 @@
 #include <Servo.h>
 Servo serv;
+boolean sensor; //Variable almacena el estado del sensor (activado/desactivado)
 
 int p = n;//n es el número del pin
 int s = m;//m es el numero del pin
 int ang = 0;
 int estado = 0;
+
+int PIR= 2 //Establece el pin 2 para el sensor PIR
+int mot= 3 //Establece el pin 3 para el motor
+int dato=1;
+	
 void apertura(int);
+void prensadobajada(int);
 void setup()
 {
 	serv.attach(s);
 	pinMode(p, INPUT);//EL PULSADOR
 	serv.write(ang);//el servo estará a cero grados
+	
+	pinMode(PIR,INPUT); //Establece el pin del sensor como entrada
+	pinMode(mot,OUTPUT); // Establece el pin del motro como salida
+        Serial.begin(9600); //Definimos la velocidad de transferencia a 9600
 
 }
 
@@ -20,9 +31,30 @@ void loop()
 	if (estado == HIGH /*&& dist.sensor>=0*/) {
 		apertura(ang);
 	}
+	
+	
+	
+	sensor=digitalRead(PIR); //Guarda el estado del sensor en la variable
+        if(sensor==HIGH) //Si el sensor es activado
+        {
+           prensadobajada(dato);
+        }
 }
 void apertura(int an) {
 	for (an = 0; an < 45; an++) {
 		serv.write(an);//se abre lentamente a 45 grados
 	}
 }
+
+void prensadobajada(int dato)
+{
+        while (Serial.available()) //Mientras el puerto serie este accesible
+        {
+            byte dato = Serial.read();  //Guardamos en dato el valor leído del puerto serie
+            if (dato==1)
+	    {  
+               digitalWrite(mot,HIGH); //Si es '1' ponemos el motor en marcha
+            }
+             
+         }
+ }
